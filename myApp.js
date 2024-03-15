124cef9bc6497160bed455a5eca2a00cd8c8870d
Olga Tsybikova <tsy2701@gmail.com>
2024-03-15 17:43:02 +0100

    Routing and middlewares

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
+require("dotenv").config();
 let express = require("express");
 let app = express();
 
 console.log("Hello World");
 
-module.exports = app;
 /* app.get("/", (req, res) => {
   res.send("Hello Express");
 }); */
@@ -14,3 +14,35 @@ app.get("/", (req, res) => {
 });
 cssPath = __dirname + "/public";
 app.use("/public", express.static(cssPath));
+/* 
+app.get("/json", (req, res) => {
+  if (process.env.MESSAGE_STYLE == "uppercase") {
+    res.json({ message: "HELLO JSON" });
+  } else {
+    res.json({ message: "Hello json" });
+  }
+}); */
+
+app.use(function (req, res, next) {
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+app.get(
+  "/now",
+  (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  },
+  (req, res) => {
+    res.send({ time: req.time });
+  }
+);
+
+app.get("/:word/echo", (req, res) => {
+  res.json({
+    echo: req.params.word,
+  });
+});
+
+module.exports = app;
